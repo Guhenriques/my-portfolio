67732bd158dbc429c1a4a00b9b7767bab13b319c
Gustavo <guhenriques1398@gmail.com>
2023-11-01 13:50:57 +0000

    portfolio all files uploaded

---
+++ emails/send_gpt.php
@@ -0,0 +1,49 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $title = $_POST['title'];
+  $message = $_POST['message'];
+
+  // Create a new PHPMailer instance
+  $mail = new PHPMailer(true);
+
+  try {
+    // SMTP Configuration
+    $mail->SMTPDebug = SMTP::DEBUG_OFF; // Disable verbose debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com'; // Set your SMTP server
+    $mail->SMTPAuth = true;
+    $mail->Username = 'guhenriques1398@gmail.com'; // SMTP username
+    $mail->Password = 'aveaqcsqyhltyopj'; // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+    $mail->Port = 465; // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    // Sender and recipient
+    $mail->setFrom('guhenriques1398@gmail.com', 'Message through my Portfolio');
+    $mail->addAddress('guhenriques1398@gmail.com', 'GustavoTo');
+    // Add more recipients or use BCC/CC if needed
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = "<p>Name: $name<br>Email: $email<br>Message: $message</p>";
+    $mail->AltBody = "Name: $name\nEmail: $email\nMessage: $message";
+
+    $mail->send();
+    echo 'Message has been sent';
+  } catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+  }
+} else {
+  echo 'Invalid request method';
+}
+?>
---
+++ js/script.js
@@ -0,0 +1,101 @@
+let gazetteerModal = $("#gazetteer-modal");
+let companyDModal = $("#companyD-modal");
+let ecommerceModal = $("#ecommerce-modal");
+
+$(document).ready(function () {
+  $('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load', function () {
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+  });
+
+  // tooltips calling after page loads
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // gazetter modal
+  $('#gazetteerProject').on('click', function () {
+    $('.main-loader').removeClass('d-none');
+    gazetteerModal.modal("show");
+    gazetteerModal.on('shown.bs.modal', function () {
+      $('.main-loader').addClass('d-none');
+    })
+  })
+
+  // companyD modal
+  $('#companyDProject').on('click', function () {
+    $('.main-loader').removeClass('d-none');
+    companyDModal.modal("show");
+    companyDModal.on('shown.bs.modal', function () {
+      $('.main-loader').addClass('d-none');
+    })
+  })
+
+  // ecommerce Modal
+  $('#ecommerceProject').on('click', function () {
+    $('.main-loader').removeClass('d-none');
+    ecommerceModal.modal("show");
+    ecommerceModal.on('shown.bs.modal', function () {
+      $('.main-loader').addClass('d-none');
+    })
+  })
+
+  // smooth scrolling
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+      500,
+      'linear'
+    );
+  });
+});
+
+
+// Handle contact form submission
+$('#emailForm').submit(function (e) {
+  e.preventDefault();
+  const formData = $(this).serialize();
+  $.ajax({
+    type: 'POST',
+    url: 'emails/send.php',
+    data: formData,
+    beforeSend: function () {
+      $('.form-send').addClass('d-none');
+      $('.preloader').addClass('d-inline');
+    },
+    success: function (response) {
+      // Parse the JSON response
+      const data = JSON.parse(response);
+
+      // Display the success modal
+      if (data.status === 'success') {
+        $('#successModal').modal('show');
+      } else {
+        $('#failedModal').modal('show');
+      }
+
+      $('#emailForm')[0].reset();
+    },
+    complete: function () {
+      setTimeout(function () {
+        $('.form-send').removeClass('d-none');
+        $('.preloader').removeClass('d-inline');
+      }, 1000);
+    },
+    error: function (xhr, status, error) {
+      $('.response-message').html('Error: ' + error);
+    }
+  });
+});
+
+// main preloader
+$(window).on('load', function () {
+  $('.main-loader').addClass('d-none');
+});
